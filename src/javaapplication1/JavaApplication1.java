9dad586216513a51ca2ee189bc50ef9d5a419306
Student <Student@L411-S09>
2018-09-28 12:13:21 +0800

    Power Method

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -14,9 +14,19 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
+    public static int Power(double base, int exponent) {
+        int total = 1;
+        for (int i = 1; i <= exponent; i++) {
+            total *= base;
+
+            
+        }
+        return total;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Fonsi");
+        int temp = Power(4,3);
+        System.out.println(temp);
     }
     
 }
